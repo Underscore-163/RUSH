39ddb5968a5683f3371ef6d3b656bc650bba27fc
Underscore-163 <reuben@dearden.org.uk>
2026-01-25 16:46:32 +0000

    fully fixed logger. added main.py. added header to sidebar.

--- client/logger.py
+++ client/logger.py
@@ -4,23 +4,26 @@ import yaml
 import os
 
 def message_test():
+    logger.debug("=====Testing logger=====")
     logger.debug ("debug message")
     logger.info ("info message")
     logger.warning ("warning message")
     logger.error ("error message")
     logger.critical ("critical message")
+    logger.debug("=====Test complete=====")
 
-if __name__ == "__main__":
-    os.chdir("data")
-    print(os.getcwd())
+
 
 print(os.getcwd())
-with open("logs/log_config.yaml") as file:
+with open("data/logs/log_config.yaml") as file:
     log_config = yaml.safe_load(file)
 dictConfig(log_config)
-logger=logging.getLogger("main")
+
+def get_main_logger():
+    return logging.getLogger("main")
 
 if __name__ == "__main__":
+    logger=logging.getLogger("main")
     message_test()
 
 
