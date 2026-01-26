80239cae4c74073b0ce9c14d2182f4fd670935d1
Underscore-163 <reuben@dearden.org.uk>
2026-01-26 17:51:37 +0000

    working sidebar framework

--- client/frontend/widgets/sidebar.py
+++ client/frontend/widgets/sidebar.py
@@ -15,40 +15,52 @@ class Sidebar:
 
         self.views = [
             Sidebar_Item(
-            master=self.sidebar,
-            sidebar=self,
-            text="Home",
-            image_path="data/assets/icons/fill/home.png",
-            position=1,
-            selection_var=self.view)
+                master=self.sidebar,
+                sidebar=self,
+                text="Home",
+                image_path="data/assets/icons/fill/home.png",
+                position=0,
+                selection_var=self.view),
+            Sidebar_Item(
+                master=self.sidebar,
+                sidebar=self,
+                text="classes",
+                image_path="data/assets/icons/fill/home.png",
+                position=1,
+                selection_var=self.view),
         ]
+
+        self.set_view(0)
 
     def change_view(self):
         print("changed view to",self.view.get())
         for view in self.views:
             if view.position is not self.view.get():
-                view.selected=False
+                view.deselect()
             else:
-                view.selected=True
                 view.select()
+
+    def set_view(self,position):
+        self.view.set(position)
+        self.change_view()
 
 
 class Sidebar_Item:
     def __init__(self,master, sidebar,text, image_path ,position, selection_var):
         self.master = master
         self.sidebar = sidebar
+        self.root = self.sidebar.master
         self.text = text
         self.image = ctk.CTkImage(Image.open(image_path))
         self.position = position
         self.selection_var = selection_var
-        self.selected=False
 
-        self.frame = ctk.CTkFrame(self.master,
-                                  fg_color="grey",
-                                  bg_color="white",
-                                  corner_radius=4)
+        self.button_frame = ctk.CTkFrame(self.master,
+                                         fg_color="grey",
+                                         bg_color="white",
+                                         corner_radius=4)
         self.button=ctk.CTkButton(
-            master=self.frame,
+            master=self.button_frame,
             command=self.clicked,
             text=self.text,
             image=self.image,
@@ -56,12 +68,22 @@ class Sidebar_Item:
             fg_color="transparent",
         )
 
-        self.frame.pack(fill="both", padx=5, pady=5)
+        self.button_frame.pack(fill="both", padx=5, pady=5)
         self.button.pack(fill="both",anchor="w")
+
+        self.frame = ctk.CTkFrame(self.root,bg_color="transparent")
+
+        ctk.CTkLabel(master=self.frame,text=self.position).pack()
 
     def clicked(self):
         self.selection_var.set(self.position)
         self.sidebar.change_view()
 
     def select(self):
-        self.frame.configure(fg_color="#e97132")
+        self.button_frame.configure(fg_color="#e97132")
+        self.frame.pack(fill="both",expand=True)
+
+    def deselect(self):
+        self.button_frame.configure(fg_color="grey")
+        self.frame.pack_forget()
+
