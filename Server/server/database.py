8a55b1cc9cdecf487ab9168151d939d1f5f699bb
Underscore-163 <reuben@dearden.org.uk>
2026-02-26 12:12:28 +0000

    simplified database.py

--- Server/server/database.py
+++ Server/server/database.py
@@ -2,6 +2,13 @@ import aiosqlite
 import os
 import asyncio
 
+async def non_returning_query(query):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
+    await cursor.execute(query)
+    await connection.commit()
+    await connection.close()
+
 async def get(table:str, item_id:int, field):
     connection = await aiosqlite.connect('data/database.db')
     cursor = await connection.cursor()
@@ -12,30 +19,17 @@ async def get(table:str, item_id:int, field):
     return data
 
 async def create(table:str, data: tuple):
-    connection = await aiosqlite.connect('data/database.db')
-    cursor = await connection.cursor()
     command=f"INSERT INTO {table} VALUES {data}"
-    await cursor.execute(command)
-    await connection.commit()
-    await connection.close()
+    asyncio.run(non_returning_query(command))
 
 async def update(table, item_id, field, data):
-    connection = await aiosqlite.connect('data/database.db')
-    cursor = await connection.cursor()
     command=f"UPDATE {table} SET {field} = '{data}'  WHERE {table[:-1]+"ID"} = {item_id}"
-    await cursor.execute(command)
-    await connection.commit()
-    await connection.close()
+    asyncio.run(non_returning_query(command))
 
 async def delete(table, item_id):
-    connection = await aiosqlite.connect('data/database.db')
-    cursor = await connection.cursor()
     command=f"DELETE FROM {table} WHERE {table[:-1]+"ID"} = {item_id}"
-    await cursor.execute(command)
-    await connection.commit()
-    await connection.close()
+    asyncio.run(non_returning_query(command))
 
 
 if __name__ == '__main__':
     os.chdir(os.getcwd().replace("server", ""))
-    print(asyncio.run(get("Users",1234,"Username")))
