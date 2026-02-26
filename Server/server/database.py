2ad6692ae85dcafe8a5ff843a799d1ee14d44a50
Underscore-163 <reuben@dearden.org.uk>
2026-02-26 11:48:34 +0000

    made database access asynchronous

--- Server/server/database.py
+++ Server/server/database.py
@@ -1,45 +1,41 @@
-import sqlite3
+import aiosqlite
 import os
+import asyncio
 
-
-
-
-
-def get(table:str, item_id:int, field):
-    connection = sqlite3.connect('data/database.db')
-    cursor = connection.cursor()
+async def get(table:str, item_id:int, field):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
     command=f"SELECT {field} FROM {table} WHERE {table[:-1]+"ID"} = {item_id}"
-    cursor.execute(command)
-    data= cursor.fetchall()
-    connection.close()
+    await cursor.execute(command)
+    data= await cursor.fetchall()
+    await connection.close()
     return data
 
-def create(table:str, data: tuple):
-    connection = sqlite3.connect('data/database.db')
-    cursor = connection.cursor()
+async def create(table:str, data: tuple):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
     command=f"INSERT INTO {table} VALUES {data}"
-    cursor.execute(command)
-    connection.commit()
-    connection.close()
+    await cursor.execute(command)
+    await connection.commit()
+    await connection.close()
 
-def update(table, item_id, field, data):
-    connection = sqlite3.connect('data/database.db')
-    cursor = connection.cursor()
+async def update(table, item_id, field, data):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
     command=f"UPDATE {table} SET {field} = '{data}'  WHERE {table[:-1]+"ID"} = {item_id}"
-    print(command)
-    cursor.execute(command)
-    connection.commit()
-    connection.close()
+    await cursor.execute(command)
+    await connection.commit()
+    await connection.close()
 
-def delete(table, item_id):
-    connection = sqlite3.connect('data/database.db')
-    cursor = connection.cursor()
+async def delete(table, item_id):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
     command=f"DELETE FROM {table} WHERE {table[:-1]+"ID"} = {item_id}"
-    cursor.execute(command)
-    connection.commit()
-    connection.close()
+    await cursor.execute(command)
+    await connection.commit()
+    await connection.close()
 
 
 if __name__ == '__main__':
     os.chdir(os.getcwd().replace("server", ""))
-    print((get("Users",1234,"Username")))
+    print(asyncio.run(get("Users",1234,"Username")))
