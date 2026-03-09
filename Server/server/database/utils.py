3a3490780caebd24a13424c548899bff321e3f24
Underscore-163 <reuben@dearden.org.uk>
2026-03-09 09:20:50 +0000

    Split database.py into multiple files in server/database

---
+++ Server/server/database/utils.py
@@ -0,0 +1,27 @@
+async def non_returning_query(query):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
+    await cursor.execute(query)
+    await connection.commit()
+    await connection.close()
+
+async def get(table:str, item_id:int, field:str="*"):
+    connection = await aiosqlite.connect('data/database.db')
+    cursor = await connection.cursor()
+    command=f"SELECT {field} FROM {table} WHERE {table[:-1]+"ID"} = {item_id}"
+    await cursor.execute(command)
+    data= await cursor.fetchall()
+    await connection.close()
+    return data
+
+async def create(table:str, fields, data):
+    command=f"INSERT INTO {table}({fields}) VALUES({data})"
+    await non_returning_query(command)
+
+async def update(table, item_id, field, data):
+    command=f"UPDATE {table} SET {field} = '{data}'  WHERE {table[:-1]+"ID"} = {item_id}"
+    await non_returning_query(command)
+
+async def delete(table, item_id):
+    command=f"DELETE FROM {table} WHERE {table[:-1]+"ID"} = {item_id}"
+    await non_returning_query(command)
