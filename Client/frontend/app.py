e27b2553aab2adf324ee432aa26a5b6511948748
Underscore-163 <reuben@dearden.org.uk>
2026-08-20 18:12:27 +0100

    Changed how the app starts for improved UX

--- Client/frontend/app.py
+++ Client/frontend/app.py
@@ -1,38 +1,31 @@
 import customtkinter as ctk
 import json
 import pywinstyles
-import time
-from frontend.widgets.frames import ContentFrame
-from frontend.widgets.styles import Fonts, Colours
-from frontend.widgets.combobutton import ComboButton
-from frontend.widgets.sidebar import Sidebar
-from frontend.widgets.file_widget import FileWidget
-from frontend.widgets.progress_tracker import ProgressTracker, VerticalTrackerBar
-from frontend.views.assignment_viewer import AssignmentViewer
-from backend import assignment_decoder
-import backend.logger as logger
+
+from Client.frontend.widgets.styles import Fonts, Colours
+from Client.frontend.widgets.sidebar import Sidebar
+from Client.frontend.views.assignment_viewer import AssignmentViewer
+from Client.backend import assignment_decoder
+import Client.backend.logger as logger
+
 log=logger.get_main_logger()
 
 class App(ctk.CTk):
     def __init__(self):
+
         ctk.CTk.__init__(self)
+        self.withdraw()
+
         self.fonts = Fonts()
         self.colours= Colours()
 
+        self.protocol("WM_DELETE_WINDOW", self.close)
 
-        pywinstyles.change_header_color(self,"#c04f15")
-        pywinstyles.change_border_color(self,"#80350e")
+        pywinstyles.change_header_color(self, "#c04f15")
+        pywinstyles.change_border_color(self, "#80350e")
         self.title("RUSH")
         self.iconbitmap("data/app_data/static/assets/RUSH_icon.ico")
         ctk.set_default_color_theme("data/app_data/static/assets/RUSH_theme.json")
-
-        self.protocol("WM_DELETE_WINDOW", self.close)
-
-
-        with open("data/app_data/dynamic/win_quit.json","r") as file:
-            win_data = json.load(file)
-            self.geometry(f"{win_data["size"]["width"]}x{win_data["size"]["height"]}+{win_data["position"]["x"]}+{win_data["position"]["y"]}")
-        
 
         self.sidebar=Sidebar(self)
         self.sidebar.pack()
@@ -41,13 +34,25 @@ class App(ctk.CTk):
         self.sidebar.add_view("Settings","data/app_data/static/assets/settings.png")
         self.sidebar.add_view("3rd Option")
 
+        with open("data/app_data/dynamic/win_quit.json","r") as file:
+            win_data = json.load(file)
+            self.geometry(f"{win_data["size"]["width"]}x{win_data["size"]["height"]}+{win_data["position"]["x"]}+{win_data["position"]["y"]}")
+            if win_data["maximised"]:
+                self.state("zoomed")
+
+        self.deiconify() #Show the window. Everything essential should be init'd before this.
+
+
         test_assign_viewer=AssignmentViewer(master=self.sidebar.get_frame("Home"),assignment=assignment_decoder.decode_assignment(r"data\user_data\assignments\test assignment.rush"))
         test_assign_viewer.pack()
 
-
+        ctk.CTkButton(master=self.sidebar.get_frame("Settings"),command=lambda: self.state("normal")).pack()
 
     def close(self):
         log.info("closing")
+        maximised=self.state()=="zoomed"
+        if maximised:
+            self.state("normal")
         with open("data/app_data/dynamic/win_quit.json","w") as file:
             win_data = {
                   "size":
@@ -56,6 +61,7 @@ class App(ctk.CTk):
                   "position":
                       {"x": self.winfo_x(),
                        "y": self.winfo_y()},
+                  "maximised":maximised
                 }
             json.dump(win_data,file)
 
